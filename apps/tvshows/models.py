2dc1b0ba5ee044f874fd9733f6fcde86d410c760
Maiya Tracy <maiya.tracy@gmail.com>
2019-06-18 16:08:48 -0700

    adding validations

--- apps/tvshows/models.py
+++ apps/tvshows/models.py
@@ -1,4 +1,29 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import datetime
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['form_title']) < 2:
+            errors['title'] = "TV show title should be at least two characters"
+        if len(postData['form_network']) < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        if (len(postData['form_description']) < 10 and len(postData['form_description']) > 0):
+            errors['description'] = "Description should be at least 10 characters"
+        if len(postData['form_release_date']) == False:
+            errors['release_date'] = "Release date cannot be empty"
+        stripped_date = datetime.strptime(postData['form_release_date'], '%Y-%m-%d')
+        if stripped_date > datetime.now():
+            errors['release_date'] = "Release date cannot be in the future"
+        return errors
+    def title_validator(self, postData):
+        errors = {}
+        entered_title = postData['form_title']
+        current_records_with_title = Show.objects.filter(title=entered_title)
+        if len(current_records_with_title) != 0:
+            errors['title'] = "Title already in the database"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length = 255)
@@ -7,5 +32,6 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
     def __repr__(self):
         return f"<User object: {self.title} {self.network} {self.release_date} {self.description} ({self.id})>"
--- apps/tvshows/views.py
+++ apps/tvshows/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Show
 import datetime
+from django.contrib import messages
 
 
 def index(request):
@@ -35,27 +36,45 @@ def edit_show(request, showid):
 
 
 def addShowToDB(request):
-    DBtitle = request.POST["form_title"]
-    DBnetwork = request.POST["form_network"]
-    DBrelease_date = request.POST["form_release_date"]
-    DBdescription = request.POST["form_description"]
-    new_show = Show.objects.create(
-        title=DBtitle, network=DBnetwork, release_date=DBrelease_date, description=DBdescription)
-    return redirect("/shows/" + str(new_show.id))
+    errors = Show.objects.basic_validator(request.POST)
+    title_errors = Show.objects.title_validator(request.POST)
+    if (len(errors) > 0 or len(title_errors) > 0):
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags=key)
+        for key, value in title_errors.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect('/shows/new')
+    else:
+        DBtitle = request.POST["form_title"]
+        DBnetwork = request.POST["form_network"]
+        DBrelease_date = request.POST["form_release_date"]
+        DBdescription = request.POST["form_description"]
+        new_show = Show.objects.create(
+            title=DBtitle, network=DBnetwork, release_date=DBrelease_date, description=DBdescription)
+        return redirect("/shows/" + str(new_show.id))
 
 
 def editShowtoDB(request, showid):
-    DBtitle = request.POST["form_title"]
-    DBnetwork = request.POST["form_network"]
-    DBrelease_date = request.POST["form_release_date"]
-    DBdescription = request.POST["form_description"]
-    show_to_edit = Show.objects.get(id=showid)
-    show_to_edit.title = DBtitle
-    show_to_edit.network = DBnetwork
-    show_to_edit.release_date = DBrelease_date
-    show_to_edit.description = DBdescription
-    show_to_edit.save()
-    return redirect("/shows/" + str(showid))
+    errors = Show.objects.basic_validator(request.POST)
+    title_errors = Show.objects.title_validator(request.POST)
+    if (len(errors) > 0 or len(title_errors) > 0):
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags=key)
+        for key, value in title_errors.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect('/shows/' + str(showid) + '/edit')
+    else:
+        DBtitle = request.POST["form_title"]
+        DBnetwork = request.POST["form_network"]
+        DBrelease_date = request.POST["form_release_date"]
+        DBdescription = request.POST["form_description"]
+        show_to_edit = Show.objects.get(id=showid)
+        show_to_edit.title = DBtitle
+        show_to_edit.network = DBnetwork
+        show_to_edit.release_date = DBrelease_date
+        show_to_edit.description = DBdescription
+        show_to_edit.save()
+        return redirect("/shows/" + str(showid))
 
 
 def delete_show(request, showid):
